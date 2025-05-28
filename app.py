c48e1a7d32e90f7edc8ce173143f0fd4e0badd74
arpitpadmani197 <arpit.padmani@theuniqueitsolution.com>
2025-05-28 17:03:51 +0530

    complted chatbot with pdf upload

--- app.py
+++ app.py
@@ -1,20 +1,13 @@
 import streamlit as st
+import os
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from langchain.llms import HuggingFaceHub
 from htmlTemplate import  css,bot_template,user_template
-from huggingface_hub import InferenceClient
-from langchain.llms import GooglePalm
-from langchain.embeddings import OpenAIEmbeddings,HuggingFaceInstructEmbeddings
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_community.llms import Ollama
-import os
-
 from langchain_community.vectorstores import FAISS
 
 def get_pdf_text(pdf_docs):
@@ -37,19 +30,12 @@ def get_text_chunks(raw_text):
 
 def get_vectorstore(text_chunks):
     load_dotenv()
-    # embedding = OpenAIEmbeddings(openai_api_type=os.getenv("OPENAI_API_KEY"))
-    # embedding = GoogleGenerativeAIEmbeddings(model="models/embedding-001",google_api_key=os.getenv("GOOGLE_API_KEY"))
     embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     vectorstore = FAISS.from_texts(texts=text_chunks,embedding=embedding)
     return vectorstore
 
 def get_conversation_chain(vector):
-    # llm = GooglePalm()
     os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
-    # llm = HuggingFaceHub(
-    #     repo_id="mistralai/Mistral-7B-Instruct-v0.1",
-    #     model_kwargs={"temperature": 0.5, "max_length": 512}
-    # )
     llm = ChatGoogleGenerativeAI(
         model="gemini-1.5-flash-latest",  # Updated model name
         temperature=0.5,
@@ -64,15 +50,16 @@ def get_conversation_chain(vector):
     return conversation_chain
 
 def handle_userinput(user_question):
-    st.write(user_question)
     if st.session_state.conversation is not None:
-        st.write(st.session_state.conversation)
         response = st.session_state.conversation({'question': user_question})
-        st.write(user_template.replace("{{MSG}}", user_question), unsafe_allow_html=True)
-        st.write(bot_template.replace("{{MSG}}", response['answer']), unsafe_allow_html=True)
+        st.session_state.chat_history=response['chat_history']
+        for i,message in enumerate(st.session_state.chat_history):
+            if i%2==0:
+                st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+            else:
+                st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
     else:
         st.warning("Please upload and process a PDF first.")
-
 
 def main():
     load_dotenv()
@@ -80,21 +67,18 @@ def main():
     st.write(css,unsafe_allow_html=True)
     if 'conversation' not in st.session_state :
         st.session_state.conversation = None
-
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history= None
     st.header("ChatBot With PDF :books:")
     user_quetion=st.text_input('Ask Question about PDF')
     if user_quetion:
         handle_userinput(user_quetion)
-
-    st.write(user_template.replace("{{MSG}}","Hello Robot"),unsafe_allow_html=True)
-    st.write(bot_template.replace("{{MSG}}","Hello User"), unsafe_allow_html=True)
 
     with st.sidebar:
         st.subheader("Your Documents")
         pdf_docs=st.file_uploader('Upload PDF Or Documents',accept_multiple_files=True)
         if st.button('Proccess'):
             with st.spinner("Proccessing"):
-
                 raw_txt=get_pdf_text(pdf_docs)
 
                 text_chunks=get_text_chunks(raw_txt)
@@ -103,7 +87,6 @@ def main():
 
                 st.session_state.conversation = get_conversation_chain(vector)
                 st.success("PDFs Processed")
-
 
 if __name__=='__main__':
     main()
